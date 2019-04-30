be3acd3c4bf8656997936f977401fab1bc95ca1a
Stzx <silence.m@hotmail.com>
2019-04-30 12:04:21 +0800

    Set the full screen mode on initialize

--- ONI-ExclusiveFullScreen/ExclusiveFullScreen.cs
+++ ONI-ExclusiveFullScreen/ExclusiveFullScreen.cs
@@ -1,16 +1,34 @@
 ﻿
+using System;
+
 using Harmony;
+
 using UnityEngine;
+
+using Internal = UnityEngine.Internal;
 
 namespace ONI_ExclusiveFullScreen
 {
 
-    [HarmonyPatch(typeof(Screen), "SetResolution", new[] { typeof(int), typeof(int), typeof(bool), typeof(int) })]
-    internal static class ExclusiveFullScreen
+    [HarmonyPatch(typeof(DistributionPlatform), "Initialize", new Type[0])]
+    internal static class HarmonyPatchDistributionPlatform
     {
-        static void Prefix(Screen __instance, int width, int height, bool fullscreen, int preferredRefreshRate)
+        static bool Prefix()
         {
-            Screen.SetResolution(width, height, fullscreen ? FullScreenMode.ExclusiveFullScreen : FullScreenMode.Windowed, preferredRefreshRate);
+            Screen.fullScreenMode = (Screen.fullScreen) ? FullScreenMode.ExclusiveFullScreen : FullScreenMode.Windowed;
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Screen), "SetResolution", new[] { typeof(int), typeof(int), typeof(bool), typeof(int) })]
+    internal static class HarmonyPatchScreen
+    {
+        static bool Prefix(Screen __instance, int width, int height, bool fullscreen, [Internal.DefaultValue("0")] int preferredRefreshRate)
+        {
+            Screen.SetResolution(width, height, (fullscreen) ? FullScreenMode.ExclusiveFullScreen : FullScreenMode.Windowed, preferredRefreshRate);
+
+            return false;
         }
     }
 }
