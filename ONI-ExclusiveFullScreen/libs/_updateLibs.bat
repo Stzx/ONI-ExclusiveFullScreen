079e6dfb7fde836c09417383603ac5ad24b013a0
Stzx <silence.m@hotmail.com>
2019-05-03 20:32:06 +0800

    Updated updateLibs.bat

--- ONI-ExclusiveFullScreen/libs/_updateLibs.bat
+++ ONI-ExclusiveFullScreen/libs/_updateLibs.bat
@@ -2,6 +2,6 @@
 
 set PATH=%ONI_PATH%\OxygenNotIncluded_Data\Managed
 
-xcopy "%PATH%\UnityEngine*.dll" %~dp0 /Y
+xcopy "%PATH%\Unity*.dll" %~dp0 /Y
 xcopy "%PATH%\Assembly-CSharp*.dll" %~dp0 /Y
 xcopy "%PATH%\0Harmony.dll" %~dp0 /Y
